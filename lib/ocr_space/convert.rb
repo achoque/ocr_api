f1cf3f63933a4acb1212555f5d1bbbd932b2c9d6
Albaro <achoque@iventiva.com>
2019-07-09 10:11:14 -0400

    Add endpoint to api request

--- lib/ocr_space/convert.rb
+++ lib/ocr_space/convert.rb
@@ -1,42 +1,44 @@
 require 'ocr_space/file_post'
 module OcrSpace
     module Convert
-        def convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil)
+        def convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil, endpoint: nil, extra_params: {})
           if file
             @files = File.new(file)
             @data = OcrSpace::FilePost.post('/parse/image',
                                             body: { apikey: apikey,
                                                     language: language,
                                                     isOverlayRequired: isOverlayRequired,
-                                                    file: @files })
+                                                    file: @files }.merge(extra_params))
             @data = @data.parsed_response['ParsedResults']
           elsif url
-            @data = HTTParty.post('https://api.ocr.space/parse/image',
+            endpoint = endpoint || "https://api.ocr.space/parse/image"
+            @data = HTTParty.post(endpoint,
                                   body: { apikey: apikey,
                                           language: language,
                                           isOverlayRequired: isOverlayRequired,
-                                          url: url })
+                                          url: url }.merge(extra_params))
             @data = @data.parsed_response['ParsedResults']
           else
             "You need to Pass either file or url."
           end
         end
 
-        def clean_convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil)
+        def clean_convert(apikey: @api_key, language: 'eng', isOverlayRequired: false, file: nil, url: nil, endpoint: nil, extra_params: {})
           if file
             @files = File.new(file)
             @data = OcrSpace::FilePost.post('/parse/image',
                                             body: { apikey: apikey,
                                                     language: language,
                                                     isOverlayRequired: isOverlayRequired,
-                                                    file: @files })
+                                                    file: @files }.merge(extra_params))
             @data = @data.parsed_response['ParsedResults'][0]["ParsedText"].gsub(/\r|\n/, "")
           elsif url
-            @data = HTTParty.post('https://api.ocr.space/parse/image',
+            endpoint = endpoint || "https://api.ocr.space/parse/image"
+            @data = HTTParty.post(endpoint,
                                   body: { apikey: apikey,
                                           language: language,
                                           isOverlayRequired: isOverlayRequired,
-                                          url: url })
+                                          url: url }.merge(extra_params))
             @data = @data.parsed_response['ParsedResults'][0]["ParsedText"].gsub(/\r|\n/, "")
           else
             "You need to Pass either file or url."
